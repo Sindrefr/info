dbd963cd943ee10567f058398dc86e504ddf36f4
sindrefr <sindre.froysa@gmail.com>
2022-10-31 13:23:14 +0100

    fixes JS

--- scripts/exhangeApp.js
+++ scripts/exhangeApp.js
@@ -2,12 +2,14 @@ const amountForm = document.querySelector("#amount");
 const fromCurrington = document.querySelector("#from");
 const toCurrington = document.querySelector("#to");
 const form = document.querySelector("#convert");
+const output = document.getElementById("output")
 
 form.addEventListener("submit", e => {
     e.preventDefault();
     from = from.value;
     to = to.value;
     amount = amount.value;
+    convert(to, from, amount)
     return from, to, amount;
 });
 
@@ -21,7 +23,9 @@ let requestOptions = {
   headers: myHeaders
 };
 
-fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,  requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+async function convert(to, from, amount){
+  fetch("https://api.apilayer.com/exchangerates_data/convert?to=" + to + "&from=" + from + "&amount=" + amount,  requestOptions)
+    .then(res => res.json())
+    .then(result => output.innerText = result.result)
+    .catch(err => console.log(err));
+}
